836ad8437367f6dd60664db950123ec7eaa4e6e4
Greg Turner <greg.fun@gmail.com>
2012-12-14 14:11:47 -0800

    Barcode scanner plugin added.

--- Agent/AppBase/js/views/ScanView.js
+++ Agent/AppBase/js/views/ScanView.js
@@ -10,6 +10,17 @@ $(function($) {
 		
 	    render:function (eventName) {
 	        $(this.el).html(this.template());
+	        window.plugins.barcodeScanner.scan(
+                function(result) {
+                    if (result.cancelled)
+                        alert("the user cancelled the scan")
+                    else
+                        alert("we got a barcode: " + result.text)
+                },
+                function(error) {
+                    alert("scanning failed: " + error)
+                }
+            )
 	        return this;
 	    },
 	
