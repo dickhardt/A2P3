c44e5ee10f1120cf277b7bcf7c7a5d95d069e22c
Dick Hardt <dickhardt@gmail.com>
2012-12-19 11:39:05 -0800

    more tests and fixes

--- server/app/as/as.js
+++ server/app/as/as.js
@@ -5,52 +5,203 @@
 */
 
 var express = require('express')
- , request = require('../request')
+  , request = require('../request')
+  , token = require('../token')
   , config = require('../config')
   , vault = require('./vault')
   , util = require('util')
   , db = require('../db')
-  , middleware = require('../middleware')
+  , mw = require('../middleware')
+  , jwt = require('../jwt')
 
+
+// called by IX when agent is to be deleted
 function agentDelete ( req, res, next ) {
     res.send(501, 'NOT IMPLEMENTED');
 }
 
+// called by app when subsequent web app login to notify personal agent
 function notify ( req, res, next ) {
     res.send(501, 'NOT IMPLEMENTED');
 }
 
+// generate an IX token for Agent
+function tokenHandler ( req, res, next ) {
+  var device = req.body.device
+    , sar = req.body.sar
+    , auth = req.body.auth
+  db.retrieveAgentFromDevice( device, function ( e, agent ) {
+    if (e) { e.code = "INTERNAL_ERROR"; return next(e) }
+    if (!agent) {
+      e = new Error('Unknown device id.')
+      e.code = "INVALID_DEVICEID"
+      return next(e)
+    }
+    if (auth.passcode && (agent.passcode != auth.passcode)) {
+      e = new Error('Invalid passcode.')
+      e.code = "INVALID_PASSCODE"
+      return next(e)
+    }
+    var payload =
+      { 'iss': config.host.as
+      , 'aud': config.host.ix
+      , 'prn': agent.prn
+      , 'token.a2p3.org': 
+        { 'sar': sar
+        , 'auth': 
+          { 'passcode': (auth.passcode) ? true : false
+          , 'authorization': (auth.authorization) ? true : false
+          , 'nfc': (auth.nfc) ? true : false
+          }
+        }
+      }
+    var ixToken = token.create( payload, vault.ix.latest )
+    return res.send( {'result': {'token': ixToken }})
+  })
+}
+
+// called from personal agent after QR code was scanned and passcode entered
 function register ( req, res, next ) {
-  var e = undefined
-  if (!req.body) e = new Error("No JSON in POST")
-  else if (!req.body.device) e = new Error("No 'device' in JSON POST")
-  else if (!req.body.passcode) e = new Error("No 'passcode' in JSON POST")
-  else if (!req.body.request) e = new Error("No 'request' in JSON POST")
-  if (e) {
-    e.code = 'INVALID_API_CALL'
-    next(e)
-    return undefined
+  var device = req.body.device
+    , passcode = req.body.passcode
+    , qr = req.body.qr
+  try {
+    var jwe = new jwt.Parse( qr )
+    var payload = jwe.decrypt( vault.keys[config.host.as].latest.key )    
+  }
+  catch (e) {
+    e.code = "INVALID_REQUEST"
+    return next( e )
   }
 
-  // TBD add code to register
+  db.retrieveAgentRegisterSession( payload['token.a2p3.org'].registerSession, function ( e, session ) {
+    if (e) { e.code = "INTERNAL_ERROR"; return next(e) }
+    if (!session) {
+      e = new Error("Unknown agent register session")
+      e.code = "INVALID_SESSION"
+      return next(e)
+    }
+    // TBD call IX to add an agent
+    var handle = '12345'
+    var agent = 
+      { 'device': session.device
+      , 'passcode': session.passcode
+      , 'handle': 'xyz'
+      }
+    db.storeAgent( agent, function ( e ) {
+      if (e) {
+        e.code = "INTERNAL_ERROR"
+        return next(e)
+      }
+      res.send({ 'response': {'token': handle}})      
+    })
+  })
 
 }
 
-function token ( req, res, next ) {
-    res.send(501, 'NOT IMPLEMENTED');
+// POST that takes register session and device passcode and returns URI to be turned into QR code
+// URI is passed back in /register
+function registerQR ( req, res, next ) {
+  var registerSession = req.body.registerSession // Registar Session Token
+  var passcode = req.body.passcode
+  db.storeAgentRegisterSessionValue( registerSession, 'passcode', passcode, function (e) {
+    if (e) { e.code = "INTERNAL_ERROR"; return next(e) }
+    var registerToken = 
+      { 'iss': config.host.as
+      , 'token.a2p3.org': { 'registerSession': registerSession }
+      }
+    var jwe = token.create( registerToken, vault.keys[config.host.as].latest)
+    var r = { 'result': { 'qr': jwe }}
+    return res.send( r )
+  })
+}
+
+// called by setup app using GET with no parameters, returns an Agent Request
+function registerRequestAgent ( req, res, next ) {
+  var registerSession = jwt.handle()
+  var agentRequest =
+    { 'iss': config.host.as
+    , 'aud': config.host.ix
+    , 'request.a2p3.org':
+     { 'resources':
+        [ config.host.ix + '/scope/register/agent' ]
+      , 'auth': 
+        { 'passcode': true
+        , 'authorization': true
+        }
+      , 'returnURL': config.baseUrl.as + '/register/agent/' + registerSession
+      }
+    }
+  var jws = request.create( agentRequest, vault.keys[config.host.ix].latest )
+  db.storeAgentRegisterSessionValue( registerSession, 'request', jws, function (e) {
+    if (e) { e.code = "INTERNAL_ERROR"; return next(e) }
+    var r = { 'result': { 'request': jws }}
+    return res.send( r )
+  })  
+}
+
+// return URL for results of authorization request to register a new personal agent
+function registerAgent ( req, res, next ) {
+  registerSession = req.params.registerSession
+  ixToken = req.query.token // IX Token
+  db.storeAgentRegisterSessionValue( registerSession, 'token', ixToken, function (e) {
+    if (e) {
+      e.code = "INTERNAL_ERROR"
+      return next(e)
+    }
+    // return agent registration page - TBD, put in final page
+    res.sendfile( '/assets/placeholder.html', { 'root': __dirname }, function (e) {
+      if (e) { e.code = "INTERNAL_ERROR"; return next(e) }
+    }) 
+  })
+}
+
+function registerAgentList ( req, res, next ) {
+  db.retrieveAgentRegisterSession( payload['request.a2p3.org'].registerSession, function ( e, session ) {
+    if (e) { e.code = "INTERNAL_ERROR"; return next(e) }
+    if (!session) {
+      e = new Error("Unknown agent register session")
+      e.code = "INVALID_SESSION"
+      return next(e)
+    }
+    // TBD call IX to get list and then send
+
+  })
+}
+
+function registerAgentDelete ( req, res, next ) {
+  db.retrieveAgentRegisterSession( payload['request.a2p3.org'].registerSession, function ( e, session ) {
+    if (e) { e.code = "INTERNAL_ERROR"; return next(e) }
+    if (!session) {
+      e = new Error("Unknown agent register session")
+      e.code = "INVALID_SESSION"
+      return next(e)
+    }
+    // TBD call IX to delete and send response
+
+  })
 }
 
 
+
+// setup AS middleware
 exports.app = function() {
 	var app = express()
 
   app.use(express.limit('10kb'))  // protect against large POST attack  
   app.use(express.bodyParser())
 
-  app.post('/agent/delete', agentDelete)
-  app.post('/notify/:handle', notify)
-  app.post('/register', register)
-  app.post('/token', token)
+  app.post('/agent/delete', request.check( vault ), agentDelete) 
+  app.post('/notify/:handle',   mw.checkParams( {'params':['handle']} ),                    notify)
+  app.post('/register',         mw.checkParams( {'body':['device','passcode','qr']} ),      register)
+  app.post('/register/qr',      mw.checkParams( {'body':['registerSession', 'passcode']} ), registerQR)
+  app.post('/token',            mw.checkParams( {'body':['device', 'sar', 'auth']} ),       tokenHandler)
+
+  app.get('/register/request/agent', registerRequestAgent)
+  app.get('/register/agent/:registerSession', mw.checkParams( {'params':['registerSession']} ), registerAgent)
+
+  app.post('/register/agent/list',    mw.checkParams( {'body':['registerSession']} ), registerAgentList)
+  app.post('/register/agent/delete',  mw.checkParams( {'body':['registerSession','handle']} ), registerAgentDelete)
 
   app.get('/', function(req, res){
     console.log(req.domain);
@@ -59,7 +210,7 @@ exports.app = function() {
       res.send(html);    
   });
 
-  app.use( middleware.errorHandler )
+  app.use( mw.errorHandler )
 
 console.log( 'AS middleware:\n', app.stack, '\nroutes:',app.routes )
 
--- server/app/db.js
+++ server/app/db.js
@@ -4,12 +4,73 @@
 * Copyright (C) Province of British Columbia, 2013
 */
 
+var config = require('./config')
+
+var dummyNoSql = {}
+
+/*
+* Registrar DB functions
+*/
 exports.validAgent = function ( token, callback ) {
   // stub for now
   process.nextTick( function () { callback( (token == 'testToken') ) } )
 }
 
+
+/*
+* IX DB calls functions
+*/
 exports.getAppName = function ( appId, callback ) {
   // stub for now
   process.nextTick( function () { callback( ('Example App') ) } )
 }
+
+
+/*
+* AS DB functions
+*/
+exports.storeAgentRegisterSessionValue = function ( id, label, data, cb ) {
+  var key = config.host.as + ':agentRegisterSession:' + id
+  dummyNoSql[key] = dummyNoSql[key] || {}
+  dummyNoSql[key][label] = data
+  process.nextTick( function () { cb( null ) } )
+}
+
+exports.retrieveAgentRegisterSession = function ( id, cb ) {
+  process.nextTick( function () {
+    var key = config.host.as + ':agentRegisterSession:' + id
+    cb( null, dummyNoSql[key] )
+  })
+}
+
+exports.storeAgent = function ( agent, cb ) {
+  var key = config.host.as + ':agent:device:' + agent.device
+  dummyNoSql[key] = agent
+  key = config.host.as + ':agent:handle:' + agent.handle
+  dummyNoSql[key] = agent.device
+  process.nextTick( function () { cb( null ) } )
+}
+
+exports.retrieveAgentFromDevice = function ( device, cb) {
+  var key = config.host.as + ':agent:device:' + device
+  var agent = dummyNoSql[key]
+  process.nextTick( function () { cb( null, agent ) } )
+}
+
+exports.retrieveAgentFromHandle = function ( handle, cb) {
+  var key = config.host.as + ':agent:handle:' + handle
+  var device = dummyNoSql[key]
+  var key = config.host.as + ':agent:device:' + device
+  var agent = dummyNoSql[key]
+  process.nextTick( function () { cb( null, agent ) } )  
+}
+
+exports.deleteAgentFromHandle = function ( handle, cb) {
+  var key = config.host.as + ':agent:handle:' + handle
+  var device = dummyNoSql[key]
+  delete dummyNoSql[key]
+  var key = config.host.as + ':agent:device:' + device
+  delete dummyNoSql[key]
+  process.nextTick( function () { cb( null ) } )  
+}
+
