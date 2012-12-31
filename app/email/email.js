0c339160c57b635cdc79daaf63609d3b896847ff
Dick Hardt <dickhardt@gmail.com>
2012-12-31 13:25:17 -0800

    factored login out to middleware module

--- app/email/email.js
+++ app/email/email.js
@@ -40,127 +40,22 @@ function emailDefault ( req, res, next ) {
   })
 }
 
-/*
-* login code
-*/
 
-function fetchIXToken ( agentRequest, ixToken, details, cb ) {
-   var apiDetails =
-      { host: 'ix'
-      , api: '/exchange'
-      , credentials: details.vault.keys[config.host.ix].latest
-      , payload: 
-        { iss: config.host[details.app]
-        , aud: config.host.ix
-        , 'request.a2p3.org':
-          { 'token': ixToken
-          , 'request': agentRequest
-          }
-        }
-      }
-    api.call( apiDetails, cb )
-}
-
-// /*/login handler, generates an Agent Request 
-// returns to caller if 'qr' parameter provided
-// redirects to 'returnURL' if provided
-// else redirects to Agent Protocol Handler
-function login ( details ) {
-  return function login ( req, res, next ) {
-    // create Agent Request
-    var agentRequestPayload =
-      { iss: config.host[details.app]
-      , aud: config.host.ix
-      , 'request.a2p3.org':
-        { 'returnURL': details.url.return 
-        , 'resources': details.resources
-        , 'auth': { 'passcode': true, 'authorization': true }
-        }
-      }
-    var jsonResponse = req.query && req.query.json
-    var agentRequest = request.create( agentRequestPayload, details.vault.keys[config.host.ix].latest )  
-    req.session.agentRequest = agentRequest
-    var redirectUrl = (req.query && req.query.returnURL) ? req.query.returnURL : 'a2p3.net://token'
-    redirectUrl += '?request=' + agentRequest
-    if (jsonResponse) {  // client wants JSON, likely will generate QR code
-      var state = jwt.handle()
-      req.session.loginState = state
-      var statusURL = details.url.return + '?' + querystring.stringify( { 'state': state } )
-      redirectUrl += '&' + querystring.stringify( { 'statusURL': statusURL, 'state': state } )
-      return res.send( { result: {'request': redirectUrl } } )
-    } else {
-      return res.redirect( redirectUrl )
-    }
-  }
-}
-
-// /*/login/return handler 
-// if gets IX Token, fetches RS Tokens and redirects to success or error urls
-function loginReturn ( details ) {
-  return function loginReturn ( req, res, next ) {
-    // check if we got IX Token
-    var ixToken = req.query.token
-    var errorCode = req.query.error
-    var errorMessage = req.query.errorMessage
-
-    function sendError ( code, message ) {
-      var errorUrl = details.url.error + '&' + querystring.stringify( {'error':code,'errorMessage':message})
-      return res.redirect( errorUrl )
-    }
-    if (!req.session.agentRequest) return sendError( "UNKNOWN", "Session information lost" )
-    if (!ixToken) return sendError( errorCode, errorMessage )
-
-    fetchIXToken( req.session.agentRequest, ixToken, details, function (response) {
-      if (response.error) return sendError( response.error.code, response.error.message )
-      req.session.di = response.result.sub
-      req.session.tokens = response.result.tokens
-      req.session.redirects = response.result.redirects
-      var jsonResponse = req.query && req.query.json
-      if (jsonResponse) {  // client wants JSON
-        return res.send( { result: {'url': details.url.success } } )
-      } else {
-        res.redirect( details.url.success )
-      }
-    })
-  }
-}
-function loginStateCheck ( details ) {
-  return function loginStateCheck ( req, res, next ) {
-    if (!req.query.state) return next()
-    // we have a loginState, which means we have moved the Agent Request
-    // and IX Token using a different browser
-
-    if (req.query.token || req.query.error) { // we are getting token or error from the agent, publish to channel
-      db.writeChannel( req.query.state, req.query )
-      res.redirect( details.url.remoteComplete )
-      return // next('route')
-    } else {
-      if (req.query.state != req.session.loginState) {
-        var e = new Error('Could not find state in session data')
-        e.code = 'UNKNOWN_ERROR'
-        return next(e)
-      }
-      db.readChannel( req.query.state, function ( e, query ) {
-        if (e) return next( e )
-        Object.keys(query).forEach( function (k) { req.query[k] = query[k] } )
-        next()
-      })
-    }
-  }
-}
 
 var loginDetails =
   { 'app': 'email'
   , 'vault': vault
+  , 'baseUrl': config.baseUrl.email
   , 'resources':
     [ config.baseUrl.email + '/email/default'
     , config.baseUrl.registrar + '/scope/verify'
     ]
-  , 'url':
-    { 'return':   config.baseUrl.email + '/dashboard/login/return'
-    , 'error':      config.baseUrl.email + '/dashboard/error'
-    , 'success':    config.baseUrl.email + '/dashboard'
-    , 'remoteComplete':    config.baseUrl.email + '/dashboard/complete'  
+  , 'path':
+    { 'login':      '/dashboard/login'
+    , 'return':     '/dashboard/login/return'
+    , 'error':      '/dashboard/error'
+    , 'success':    '/dashboard'
+    , 'complete':   '/dashboard/complete'  
     }
   }
 
@@ -176,9 +71,7 @@ exports.app = function() {
   
   registration.routes( app, 'email', vault )  // add in routes for the registration paths
 
-  app.get('/dashboard/login', login( loginDetails ) )
-
-  app.get('/dashboard/login/return', loginStateCheck( loginDetails ), loginReturn( loginDetails ) )
+  mw.loginHandler( app, loginDetails )
 
   app.post('/di/link' 
           , request.check( vault.keys, config.roles.enroll )
--- app/middleware.js
+++ app/middleware.js
@@ -7,6 +7,14 @@
 */
 
 // debugging middleware to trace execution
+
+var config = require('./config')
+  , request = require('./request')
+  , api = require('./api')
+  , jwt = require('./jwt')
+  , querystring = require('querystring')
+  , db = require('./db')
+
 exports.trace = function trace ( req, res, next ) {
   console.log('TRACE:',req.host,req.originalUrl)
   next()
@@ -107,4 +115,156 @@ exports.colorLogger = function colorLogger ( express ) {
     })
 
   return express.logger( ':wideHost\t:method\t:url\t:statusColor\t:response-time\tms\t:errorCode\t:errorMessage' )
+}
+
+/*
+* login middleware
+*
+* general middleware for logging into a site and getting resource tokens
+*/ 
+
+
+function fetchIXToken ( agentRequest, ixToken, details, cb ) {
+   var apiDetails =
+      { host: 'ix'
+      , api: '/exchange'
+      , credentials: details.vault.keys[config.host.ix].latest
+      , payload: 
+        { iss: config.host[details.app]
+        , aud: config.host.ix
+        , 'request.a2p3.org':
+          { 'token': ixToken
+          , 'request': agentRequest
+          }
+        }
+      }
+    api.call( apiDetails, cb )
+}
+
+// /*/login handler, generates an Agent Request 
+// returns to caller if 'qr' parameter provided
+// redirects to 'returnURL' if provided
+// else redirects to Agent Protocol Handler
+function login ( details ) {
+  return function login ( req, res, next ) {
+    // create Agent Request
+    var agentRequestPayload =
+      { iss: config.host[details.app]
+      , aud: config.host.ix
+      , 'request.a2p3.org':
+        { 'returnURL': details.url.return 
+        , 'resources': details.resources
+        , 'auth': { 'passcode': true, 'authorization': true }
+        }
+      }
+    var jsonResponse = req.query && req.query.json
+    var agentRequest = request.create( agentRequestPayload, details.vault.keys[config.host.ix].latest )  
+    req.session.agentRequest = agentRequest
+    var redirectUrl = (req.query && req.query.returnURL) ? req.query.returnURL : 'a2p3.net://token'
+    redirectUrl += '?request=' + agentRequest
+    if (jsonResponse) {  // client wants JSON, likely will generate QR code
+      var state = jwt.handle()
+      req.session.loginState = state
+      var statusURL = details.url.return + '?' + querystring.stringify( { 'state': state } )
+      redirectUrl += '&' + querystring.stringify( { 'statusURL': statusURL, 'state': state } )
+      return res.send( { result: {'request': redirectUrl } } )
+    } else {
+      return res.redirect( redirectUrl )
+    }
+  }
+}
+
+// /*/login/return handler 
+// if gets IX Token, fetches RS Tokens and redirects to success or error urls
+function loginReturn ( details ) {
+  return function loginReturn ( req, res, next ) {
+    // check if we got IX Token
+    var ixToken = req.query.token
+    var errorCode = req.query.error
+    var errorMessage = req.query.errorMessage
+
+    function sendError ( code, message ) {
+      var errorUrl = details.url.error + '&' + querystring.stringify( {'error':code,'errorMessage':message})
+      return res.redirect( errorUrl )
+    }
+    if (!req.session.agentRequest) return sendError( "UNKNOWN", "Session information lost" )
+    if (!ixToken) return sendError( errorCode, errorMessage )
+
+    fetchIXToken( req.session.agentRequest, ixToken, details, function (response) {
+      if (response.error) return sendError( response.error.code, response.error.message )
+      req.session.di = response.result.sub
+      req.session.tokens = response.result.tokens
+      req.session.redirects = response.result.redirects
+      var jsonResponse = req.query && req.query.json
+      if (jsonResponse) {  // client wants JSON
+        return res.send( { result: {'url': details.url.success } } )
+      } else {
+        res.redirect( details.url.success )
+      }
+    })
+  }
+}
+function loginStateCheck ( details ) {
+  return function loginStateCheck ( req, res, next ) {
+    if (!req.query.state) return next()
+    // we have a loginState, which means we have moved the Agent Request
+    // and IX Token using a different browser
+
+    if (req.query.token || req.query.error) { // we are getting token or error from the agent, publish to channel
+      db.writeChannel( req.query.state, req.query )
+      res.redirect( details.url.complete )
+      return // next('route')
+    } else {
+      if (req.query.state != req.session.loginState) {
+        var e = new Error('Could not find state in session data')
+        e.code = 'UNKNOWN_ERROR'
+        return next(e)
+      }
+      db.readChannel( req.query.state, function ( e, query ) {
+        if (e) return next( e )
+        Object.keys(query).forEach( function (k) { req.query[k] = query[k] } )
+        next()
+      })
+    }
+  }
+}
+
+/*
+
+Sample details object
+
+var details =
+  { 'app': 'email'  // app
+  , 'vault': vault  // vault for app
+  , 'resources':    // array of resources wanted by app
+    [ config.baseUrl.email + '/email/default'
+    , config.baseUrl.registrar + '/scope/verify'
+    ]
+  , 'path':          // paths were each step of login goes
+        // these paths are managed here 
+    { 'login':      '/dashboard/login'
+    , 'return':     '/dashboard/login/return'
+        // these are static pages that should map to somewhere TBD, generic?
+    , 'error':      '/dashboard/error'        
+    , 'success':    '/dashboard'
+    , 'complete':   '/dashboard/complete'  
+    }
+  }
+
+*/
+
+exports.loginHandler = function ( app, detailsOrig ) {
+
+  // clone object as we are going to muck with it
+  var details = JSON.parse(JSON.stringify(detailsOrig))
+  // create URLs to use
+  details.url = {}
+  Object.keys( details.path ).forEach ( function ( p ) {
+    details.url[p] = details.baseUrl + details.path[p] 
+  })
+
+  app.get( details.path.login, login( details ) )
+
+  app.get( details.path.return, loginStateCheck( details ), loginReturn( details ) )
+
 }
