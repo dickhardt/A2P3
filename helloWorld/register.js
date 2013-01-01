0cc246726b0e5c65196e5d65219f830f754abc18
Dick Hardt <dickhardt@gmail.com>
2012-12-31 18:17:15 -0800

    factored out login code

--- helloWorld/register.js
+++ helloWorld/register.js
@@ -22,7 +22,43 @@ var setupDI       // root user Directed Identifier, fetched from Setup Agent sto
   , rsTokens
   , cookieJar
 
-tasks =
+
+function dashboardLogin ( di, rs, vault, done ) {
+  var cookieJar
+    , agentRequest
+  
+  var options = { disableRedirects: true }
+  fetchUrl( config.baseUrl[rs] + '/dashboard/login', options, function ( error, meta, body ) {
+    if ( error ) return done( error )
+    cookieJar = meta.cookieJar
+    var o = url.parse( meta.responseHeaders.location, true )
+    agentRequest = o.query.request
+    var jws = new jwt.Parse( agentRequest )
+    var tokenPayload =
+      { iss: config.host.setup
+      , aud: config.host.ix
+      , sub: setupDI
+      , 'token.a2p3.org':
+        { 'auth': jws.payload['request.a2p3.org'].auth
+        , 'sar': jws.signature
+        }
+      }
+    var ixToken = token.create( tokenPayload, vault.keys[config.host.ix].latest )
+    var returnURL = jws.payload['request.a2p3.org'].returnURL + '?token=' + ixToken + '&json=true'
+    var options = { disableRedirects: true, cookieJar: cookieJar }
+    fetchUrl( returnURL, options, function ( error, meta, body ) {
+      if ( error ) return done( error )
+      var result = JSON.parse( body )
+      if (result.error) return done( result.error )
+      cookieJar = meta.cookieJar
+console.log('we are now logged into '+config.host[rs])      
+      done( null, cookieJar )
+    })
+  })
+}
+
+
+var tasks =
   [ function getDI ( done ) {
       db.retrieveAgentFromDevice( 'root', function ( e, agent ) {
         if ( e ) return done( e ) 
@@ -31,41 +67,13 @@ console.log('setupDI',setupDI)
         done( null, 'ok' )
       })
     }
-  , function registrarGetAgentRequest ( done ) {
-      var options = { disableRedirects: true }
-      fetchUrl( config.baseUrl.registrar + '/dashboard/login', options, function ( error, meta, body ) {
-        if ( error ) return done( error )
-        cookieJar = meta.cookieJar
-        var o = url.parse( meta.responseHeaders.location, true )
-        agentRequest = o.query.request
-console.log('agentRequest',agentRequest)        
-        done( null, 'ok' )
-      })
-    } 
   , function registrarLogin ( done ) {
-      var jws = new jwt.Parse( agentRequest )
-      var tokenPayload =
-        { iss: config.host.setup
-        , aud: config.host.ix
-        , sub: setupDI
-        , 'token.a2p3.org':
-          { 'auth': jws.payload['request.a2p3.org'].auth
-          , 'sar': jws.signature
-          }
-        }
-      var ixToken = token.create( tokenPayload, vaultSetup.keys[config.host.ix].latest )
-      var returnURL = jws.payload['request.a2p3.org'].returnURL + '?token=' + ixToken + '&json=true'
-      var options = { disableRedirects: true, cookieJar: cookieJar }
-      fetchUrl( returnURL, options, function ( error, meta, body ) {
-        if ( error ) return done( error )
-        var result = JSON.parse( body )
-        if (result.error) return done( result.error )
-        cookieJar = meta.cookieJar
-console.log('we are now logged in')      
-        done( null, 'ok' )
+      dashboardLogin( setupDI, 'registrar', vaultSetup, function ( e, newCookies ) {
+        if (e) return done( e )
+        cookieJar = newCookies
       })
     }
-  ]  
+  ]
 
 async.series( tasks, function ( error, results ) {
   console.log('error:',error)
