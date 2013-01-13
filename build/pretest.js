8695e4d6b625ed9da93b455c06deaafad40adec4
Dick Hardt <dickhardt@gmail.com>
2013-01-13 11:43:52 -0800

    updated testing so it should work on Windoze

--- build/pretest.js
+++ build/pretest.js
@@ -6,15 +6,24 @@
 
 var fetch = require('request')
   , config = require('../app/config')
+  , exec = require('child_process').exec
 
-// fetch favicon.ico to make sure server is running
-var url = config.baseUrl.setup + '/favicon.ico'
-fetch( url, function( error, meta, body ) {
+// check that mocha is installed so can give useful error message if not
+exec( 'mocha --version', function ( error ) {
   if (error) {
-    console.error('Server must be running before testing.')
-    console.error('Try "npm start".')
-    process.exit(1)
-  } else {
-    process.exit(0)
+    console.error('Does not look like you have Mocha installed globally')
+    console.error('Try "npm install mocha -g".\n')
+    return process.exit(1)
   }
+  // fetch favicon.ico to make sure server is running
+  var url = config.baseUrl.setup + '/favicon.ico'
+  fetch( url, function( error ) {
+    if (error) {
+      console.error('Server must be running before testing.')
+      console.error('Try "npm start" in a different console.\n')
+      return process.exit(1)
+    }
+    process.exit(0)
+  })
 })
+
